23642afcf9c11e5ffd5b60a2be6c9dc01e01deec
yupengzh-intel <yupeng.zhang@intel.com>
2021-08-13 09:42:55 +0800

    add wrapper to old api

---
+++ core/src/api/xpum_api.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <iostream>
+#include "xpum_api.h"
+#include "api.h"
+
+using namespace std;
+
+vector<xpum_device_basic_info> deviceInfoList;
+
+xpum_result_t xpumInit() {
+    bool res = init();
+    if(res) return XPUM_OK;
+    return XPUM_GENERIC_ERROR;
+}
+
+xpum_result_t xpumShutdown() {
+    return XPUM_OK;
+}
+
+xpum_result_t xpumGetDeviceList(xpum_device_basic_info deviceList[XPUM_MAX_NUM_DEVICES], int *count) {
+    
+    Api_result_t result;
+
+    deviceInfoList.clear();
+    
+    auto callback = [](Device_t *device)
+    {
+        xpum_device_basic_info info;
+
+        info.deviceId = stoi(device->device_id);
+
+        info.type = GPU;
+
+        for (int i = 0; i < device->property_len; i++)
+        {
+            auto &prop = device->properties[i];
+            
+            cout<<prop.name<<":\t"<<prop.value<<endl;
+        }
+
+        deviceInfoList.push_back(info);
+    };
+
+    getDeviceList(callback, &result);
+
+    *count = deviceInfoList.size();
+    
+    for(int i=0;i<deviceInfoList.size();i++){
+        deviceList[i] = deviceInfoList[i];
+    }
+
+    return XPUM_OK;
+
+}
