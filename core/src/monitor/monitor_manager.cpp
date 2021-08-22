0b4690dbf21e59118865f69842eb21e8cae926d1
Zhang Wei <wei3.zhang@intel.com>
2021-08-22 22:15:03 +0800

    remove debug some source code

--- core/src/monitor/monitor_manager.cpp
+++ core/src/monitor/monitor_manager.cpp
@@ -34,7 +34,6 @@ void MonitorManager::close() {
 }
 
 void MonitorManager::createMonitorTasks() {
-  /*
   tasks.emplace_back(std::make_shared<MonitorTask>(DeviceCapability::POWER, 
     Configuration::TELEMETRY_DATA_MONITOR_FREQUENCE, p_device_manager, p_data_logic));
   tasks.emplace_back(std::make_shared<MonitorTask>(DeviceCapability::FREQUENCY, 
@@ -45,7 +44,6 @@ void MonitorManager::createMonitorTasks() {
     Configuration::TELEMETRY_DATA_MONITOR_FREQUENCE, p_device_manager, p_data_logic));
   tasks.emplace_back(std::make_shared<MonitorTask>(DeviceCapability::ENGINE_UTILIZATION, 
     Configuration::TELEMETRY_DATA_MONITOR_FREQUENCE, p_device_manager, p_data_logic));
-    */
 
   tasks.emplace_back(std::make_shared<MonitorTask>(DeviceCapability::METRIC_TEMPERATURE, 
     Configuration::TELEMETRY_DATA_MONITOR_FREQUENCE, p_device_manager, p_data_logic, MonitorTaskType::GPU_METRICS));
