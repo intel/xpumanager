bd1b7576856480a2d6821858ffc3ede3a9c7ae7d
Wang, Yu W <yu.w.wang@intel.com>
2021-09-02 22:36:01 +0800

    enable parallel make

--- build.sh
+++ build.sh
@@ -17,14 +17,14 @@ if [ -f "/etc/debian_version" ]; then
     mkdir build-deb
     cd build-deb
     cmake ..  $@
-    make
+    make -j
     cpack    
 elif [ -f "/etc/redhat-release" ] || [ -f "/etc/SUSE-release" ]; then
     rm -rf build-rpm
     mkdir build-rpm
     cd build-rpm
     cmake ..  $@
-    make
+    make -j
     cpack
 fi
 
