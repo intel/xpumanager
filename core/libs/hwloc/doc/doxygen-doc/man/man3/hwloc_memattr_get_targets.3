8bea3f8937585783a0e5851dda8295af0d0e8f5f
sjia <shuisheng.jia@intel.com>
2021-09-03 11:06:16 -0400

    delte hwloc for too many untracked files

--- core/libs/hwloc/doc/doxygen-doc/man/man3/hwloc_memattr_get_targets.3
+++
@@ -1,1 +0,0 @@
-.so man3/hwlocality_memattrs_manage.3
--- core/libs/hwloc/tests/hwloc/ports/include/freebsd/sys/thr.h
+++
@@ -1,6 +0,0 @@
-#ifndef HWLOC_PORT_FREEBSD_SYS_THR_H
-#define HWLOC_PORT_FREEBSD_SYS_THR_H
-
-int thr_self(long *id);
-
-#endif /* HWLOC_PORT_FREEBSD_SYS_THR_H */
