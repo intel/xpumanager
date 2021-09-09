2761a9df9b5e17ba891e0ed88b4737dcd5be2b46
sjia <shuisheng.jia@intel.com>
2021-09-09 12:28:08 +0800

    change for build hwloc

--- core/libs/hwloc/build.sh
+++ core/libs/hwloc/build.sh
@@ -7,9 +7,14 @@ echo "WORK_DIR=${WORK_DIR}"
 if [ ! -f $WORK_DIR/install/lib/libhwloc.a ]; then
     echo "build hwloc"
     cd ${WORK_DIR}
-    ./autogen.sh --with-pic
+    echo "------------------------hwloc autogen---------------------------" > hwloc.log  2>&1
+    ./autogen.sh --with-pic >> hwloc.log  2>&1
     cd ${WORK_DIR}
-    ./configure  --prefix=$WORK_DIR/install --enable-static --disable-shared LDFLAGS="--static" CFLAGS="-fPIC"
-    make -j
-    make install
+    echo "------------------------hwloc configure---------------------------" >> hwloc.log  2>&1
+    ./configure  --prefix=$WORK_DIR/install --enable-static --disable-shared LDFLAGS="--static" CFLAGS="-fPIC" >> hwloc.log  2>&1
+    echo "------------------------hwloc make---------------------------" >> hwloc.log  2>&1
+    make -j >> hwloc.log  2>&1
+    echo "------------------------hwloc install---------------------------" >> hwloc.log  2>&1
+    make install >> hwloc.log  2>&1
+    echo "------------------------hwloc end---------------------------" >> hwloc.log  2>&1
 fi
