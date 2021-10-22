3d4d12466395ed44399da662b3ff1061dbbdf6a8
sjia <shuisheng.jia@intel.com>
2021-10-22 15:26:43 +0800

    installation for doc

--- doc/build.sh
+++ doc/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+WORK=`dirname "$0"`
+WORK_DIR=`cd ${WORK} && pwd`
+
 rm -rf build
 
 mkdir build
@@ -7,6 +10,7 @@ mkdir build
 # generate xml using doxygen
 doxygen Doxyfile
 
+cd ${WORK_DIR}
 # convert xml to rst by doxyrest
 DOXYREST_FOLDER="./doxyrest"
 
@@ -17,5 +21,6 @@ $DOXYREST_FOLDER/bin/doxyrest \
     -o build/rst/index.rst \
     build/doxygen-output/xml/index.xml
 
+cd ${WORK_DIR}
 # build rst to html by sphinx
 sphinx-build -c sphinxconf/ build/rst/ build/html
