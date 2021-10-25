ffd14d1f265ad9ab4685eed3a315ade05103bde3
fmiao <fabiao.miao@intel.com>
2021-10-25 13:15:28 +0800

    fix typo in comment

--- core/src/diagnostic/diagnostic_manager.h
+++ core/src/diagnostic/diagnostic_manager.h
@@ -17,7 +17,7 @@
 
 /**
  * The class is responsible for GPU diagnostics. Three levels are currently supported. It will check 
- * environment variables, libraries, permission, PCIe, media encoder and performance. And each task 
+ * environment variables, libraries, permission, PCIe, media coder and performance. And each task 
  * will be assigned a thread.
  *
  */
