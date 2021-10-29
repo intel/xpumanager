ff8b5369ad087e0830260b87c921b61077f0de5a
Yu Wang <yu.w.wang@intel.com>
2021-10-29 23:35:02 +0800

    run monitoring tasks in caller threads

--- core/src/infrastructure/thread_pool.cpp
+++ core/src/infrastructure/thread_pool.cpp
@@ -7,7 +7,7 @@
 
 namespace xpum {
 
-ThreadPool::ThreadPool(unsigned int size) : stop(false), size(size) {
+ThreadPool::ThreadPool(unsigned int size) {
     XPUM_LOG_DEBUG("ThreadPool()");
     init();
 }
@@ -18,45 +18,10 @@ ThreadPool::~ThreadPool() {
 }
 
 void ThreadPool::init() {
-    if (stop) {
-        return;
-    }
 
-    for (unsigned int i = 0; i < size; ++i) {
-        workers.emplace_back(std::thread([this, i] {
-            while (!stop) {
-                std::function<void()> task = this->tasks.remove();
-                if (task == nullptr) {
-                    continue;
-                }
-
-                try {
-                    task();
-                } catch (std::exception& e) {
-                    std::string error = "Failed to execute task in thread pool: ";
-                    error += e.what();
-                    XPUM_LOG_ERROR(error);
-                } catch (...) {
-                    std::string error = "Failed to execute task in thread pool: unexpected exception";
-                    XPUM_LOG_ERROR(error);
-                }
-            }
-        }));
-    }
 }
 
 void ThreadPool::close() {
-    if (stop) {
-        return;
-    }
-
-    stop = true;
-    tasks.close();
-
-    for (std::thread& worker : workers) {
-        worker.join();
-    }
-    workers.clear();
 }
 
 } // end namespace xpum
--- core/src/monitor/monitor_task.cpp
+++ core/src/monitor/monitor_task.cpp
@@ -58,12 +58,11 @@ void MonitorTask::start() {
             return;
         }
 
-        std::atomic<int> count(devices.size());
         auto datas = std::make_shared<std::map<std::string, std::shared_ptr<MeasurementData>>>();
         
         for (auto& p_device : devices) {
             auto method = Utility::getDeviceMethod(p_this->capability, p_device.get());
-            method([p_device, this_weak_ptr, &count, datas](
+            method([p_device, this_weak_ptr, datas](
                        std::shared_ptr<void> ret, std::shared_ptr<BaseException> e) {
                 auto p_this = this_weak_ptr.lock();
                 if (p_this == nullptr) {
@@ -73,28 +72,10 @@ void MonitorTask::start() {
                     std::string id = p_device->getId();
                     (*datas)[id] = std::static_pointer_cast<MeasurementData>(ret);
                 }
-
-                count--;
-                if (count == 0) {
-                    p_this->data_cv.notify_all();
-                }
             });
         }
 
         std::unique_lock<std::mutex> lock(p_this->mutex);
-        while (count > 0) {
-            if (p_this->data_cv.wait_for(lock, std::chrono::milliseconds(p_this->freq)) ==
-                std::cv_status::timeout) {
-                if (count > 0) {
-                    std::string error = std::string("Monitor:") + std::to_string((int)(p_this->capability)) + " is expired!";
-                    XPUM_LOG_DEBUG(error);
-                }
-                // TODO: the 'break' following is temperorily commented out to avoid memory corruption due to callback after task expiration. 
-                // need to redesign the timeout processing properly.
-                // break;
-            }
-        }
-
         MeasurementType measurmentType = Utility::measurementTypeFromCapability(p_this->capability);
         p_this->p_data_logic->storeMeasurementData(measurmentType, now, datas);
     });
