b03499e7d7843c3f4ff3719e91341ea8770147fc
Zhang Wei <wei3.zhang@intel.com>
2021-11-05 10:10:38 +0800

    Update log type in policy manager.

--- core/src/policy/policy_manager.cpp
+++ core/src/policy/policy_manager.cpp
@@ -56,12 +56,12 @@ PolicyManager::PolicyManager(std::shared_ptr<DeviceManagerInterface>& p_device_m
                              std::shared_ptr<DataLogicInterface>& p_data_logic,
                              std::shared_ptr<GroupManagerInterface>& p_group_manager)
     : p_device_manager(p_device_manager), p_data_logic(p_data_logic), p_group_manager(p_group_manager) {
-    XPUM_LOG_INFO("PolicyManager()");
+    XPUM_LOG_DEBUG("PolicyManager()");
     this->freq = Configuration::TELEMETRY_DATA_MONITOR_FREQUENCE;
 }
 
 PolicyManager::~PolicyManager() {
-    XPUM_LOG_INFO("~PolicyManager()");
+    XPUM_LOG_DEBUG("~PolicyManager()");
 }
 
 void PolicyManager::init() {
