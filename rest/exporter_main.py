a6bfb99a61fbc01c4f96a6e9724c62a84de0f96e
Yu Wang <yu.w.wang@intel.com>
2021-11-05 14:44:01 +0800

    support --socket_file

--- rest/exporter_main.py
+++ rest/exporter_main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import argparse
-import os
 from flask import Flask
 from views import exporter
 
@@ -12,6 +10,12 @@ app.add_url_rule('/metrics',
 
 if __name__ == '__main__':
 
+    import argparse
+    import os
+    import xpum_logger as logger
+
+    logger.info('XPU Manager Prometheus Exporter Started')
+
     parser = argparse.ArgumentParser(
         description='Intel XPU Manager Prometheus Exporter')
 
@@ -21,10 +25,17 @@ if __name__ == '__main__':
                         help='port on which the server listens')
     parser.add_argument('--pod', action='store_true', default=None,
                         help='enable pod information retrieval from kubernetes')
+    parser.add_argument('--socket_file', default=None,
+                        help='socket file used by xpum daemon')
 
     args = parser.parse_args()
 
     if args.pod is not None:
         os.environ['XPUM_EXPORTER_POD'] = '1' if args.pod else '0'
 
+    if args.socket_file is not None:
+        os.environ['XPUM_SOCKET_FILE'] = args.socket_file
+
     app.run(host=args.host, port=args.port, use_reloader=False)
+
+    logger.info('XPU Manager Prometheus Exporter Exited')
