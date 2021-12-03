bc8d8ac112d6c8e92fe04b9e520859d4c2d66740
Wang, Yu W <yu.w.wang@intel.com>
2021-12-03 09:48:25 +0800

    use spdlog for daemon log

--- daemon/logger.h
+++ daemon/logger.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <string>
 
 #include "spdlog/cfg/env.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 
 namespace xpum::daemon {
@@ -13,8 +16,21 @@ namespace xpum::daemon {
 
 class Logger {
    public:
-    static void init() {
+    static void init(const char* log_file_name = nullptr, std::size_t max_size = 10 * 1024 * 1024, std::size_t max_files = 3, const std::string log_level = "info") {
+        std::vector<spdlog::sink_ptr> sinks;
+        sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        if (log_file_name != nullptr) {
+            sinks.emplace_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file_name, max_size, max_files, false));
+        }
+        auto logger = std::make_shared<spdlog::logger>("daemon", begin(sinks), end(sinks));
+        logger->flush_on(spdlog::level::warn);
+        spdlog::flush_every(std::chrono::seconds(3));
+        spdlog::set_default_logger(logger);
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%L%$] [%P-%t] %v");
         spdlog::cfg::load_env_levels();
     }
+    static void close() {
+        spdlog::shutdown();
+    }
 };
-} // end namespace xpum
+} // namespace xpum::daemon
