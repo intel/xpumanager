7413404f04a10e05d4190ab15d86adb19fb1721d
sjia <shuisheng.jia@intel.com>
2021-12-04 12:01:27 +0800

    for pcie card lost check

--- core/src/topology/hwinfo.cpp
+++ core/src/topology/hwinfo.cpp
@@ -1,6 +1,11 @@
 #include "hwinfo.h"
 
 #include <experimental/filesystem>
+#include "hwloc.h"
+
+#include "infrastructure/exception/ilegal_parameter_exception.h"
+#include "infrastructure/property.h"
+#include "core/core.h"
 
 namespace xpum {
 
@@ -22,4 +27,46 @@ std::string HWInfo::getDevicePath(const std::string& bdf_address) {
 
     return result;
 }
+
+bool HWInfo::isPcieDevExist(xpum_device_id_t deviceId){
+    Property prop;
+    std::shared_ptr<Device> p_device = Core::instance().getDeviceManager()->getDevice(std::to_string(deviceId));
+    if (p_device == nullptr) {
+        XPUM_LOG_ERROR("isPcieDevExist, device {} not exist", deviceId);
+        throw IlegalParameterException("device does not exist");
+    }
+    if(!p_device->getProperty(XPUM_DEVICE_PROPERTY_PCI_BDF_ADDRESS, prop)){
+
+        throw BaseException(ErrorCode::UNKNOWN, "PCI_BDF_ADDRESS not exist");
+    }
+    bool find = false;
+    std::string bdfAddress = prop.getValue();
+    std::size_t start = 0, pos = 0;
+    int32_t domain = std::stoi(bdfAddress.substr(start), &pos, 16);
+    start += pos + 1;
+    pos = 0;
+    int32_t bus = std::stoi(bdfAddress.substr(start), &pos, 16);
+    start += pos + 1;
+    pos = 0;
+    int32_t device = std::stoi(bdfAddress.substr(start), &pos, 16);
+    start += pos + 1;
+    pos = 0;
+    int32_t function = std::stoi(bdfAddress.substr(start), &pos, 16);
+
+    hwloc_topology_t hwtopology;
+    hwloc_obj_t obj = nullptr;
+    hwloc_topology_init(&hwtopology);
+    hwloc_topology_set_io_types_filter(hwtopology, HWLOC_TYPE_FILTER_KEEP_ALL);
+    hwloc_topology_load(hwtopology);
+    while ((obj = hwloc_get_next_pcidev(hwtopology, obj)) != nullptr) {
+        assert(obj->type == HWLOC_OBJ_PCI_DEVICE);
+        if (obj->attr->pcidev.domain == domain && obj->attr->pcidev.bus == bus && obj->attr->pcidev.dev == device && obj->attr->pcidev.func == function) {
+            find = true;
+            break;
+        }
+    }
+    hwloc_topology_destroy(hwtopology);
+    return find;
+}
+
 } // end namespace xpum
