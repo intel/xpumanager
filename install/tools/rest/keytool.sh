0f609169be51bf8176ae3b171bca4e1038ea2b88
Wang, Yu W <yu.w.wang@intel.com>
2021-12-10 19:48:22 +0800

    support setting owner/group for keys&config files

--- install/tools/rest/keytool.sh
+++ install/tools/rest/keytool.sh
@@ -2,8 +2,26 @@
 
 WORK=`dirname "$0"`
 WORK_DIR=`cd ${WORK} && pwd`
-
+echo "WORK_DIR: ${WORK_DIR}"
+OWNER=xpum
+GROUP=xpum
+for i in "$@"; do
+  case $i in
+    -o=*|--owner=*)
+      OWNER="${i#*=}"
+      shift # past argument=value
+      ;;
+    -g=*|--group=*)
+      GROUP="${i#*=}"
+      shift # past argument=value
+      ;;
+    *)
+      # unknown option
+      ;;
+  esac
+done
 umask 007
 openssl req -x509 -sha512 -newkey rsa:3072 -nodes -keyout rest/conf/key.pem -out rest/conf/cert.pem -days 548 -subj "/ST=Unknown/O=XPUM"
-chown xpum:xpum rest/conf/key.pem
-chown xpum:xpum rest/conf/cert.pem
+echo "Set key file owner:group as ${OWNER}:${GROUP}"
+chown ${OWNER}:${GROUP} rest/conf/key.pem
+chown ${OWNER}:${GROUP} rest/conf/cert.pem
--- install/tools/rest/rest_config.py
+++ install/tools/rest/rest_config.py
@@ -1,20 +1,29 @@
+#!/bin/env python3
 import configparser
 import os
 import pwd
 import grp
-import sys
 import hashlib
 import getpass
+import argparse
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "-o", "--owner", help="owner of the generated config file" )
+    parser.add_argument( "-g", "--group", help="group of the generated config file" )
+    args = parser.parse_args()
+    owner = args.owner if args.owner else 'xpum'
+    group = args.group if args.group else 'xpum'
+
     print( "Please enter password for REST API user xpumadmin" )
     while True:
-        pw = getpass.getpass( 'enter password:' )
-        pwAgain = getpass.getpass( 'reenter password:' )
+        pw = getpass.getpass( 'Enter password:' )
+        pwAgain = getpass.getpass( 'Confirm password:' )
         if pw == pwAgain:
             break
         else:
-            print( 'The two passwords you enter are inconsistent' )
+            print( 'The two passwords you entered are inconsistent' )
 
     salt = os.popen( 'tr -dc A-Za-z0-9 < /dev/urandom |head -c 12' ).read()
     pwHash = hashlib.pbkdf2_hmac('sha512', pw.encode('ASCII'), salt.encode('ASCII'), 10000).hex()
@@ -25,11 +34,11 @@ if __name__ == '__main__':
     config['default']['salt'] = salt
     config['default']['password'] = pwHash
     
-    path = os.path.dirname( os.path.realpath( __file__ ) )
+    path = os.getcwd()
     os.umask( 0o007 )
     with open( path + '/rest/conf/rest.conf', 'w' ) as configFile:
         config.write( configFile )
-    os.chown( path + '/rest/conf/rest.conf', pwd.getpwnam( 'xpum' ).pw_uid, grp.getgrnam( 'xpum' ).gr_gid )
+    os.chown( path + '/rest/conf/rest.conf', pwd.getpwnam( owner ).pw_uid, grp.getgrnam( group ).gr_gid )
 
     print( 'rest.conf has been generated succefully' )
     print( 'If you forget the password, just re-run this script again' )
