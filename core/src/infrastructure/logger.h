0e63498859aa8501faef5ab61f823781c10c60c9
Wang, Yu W <yu.w.wang@intel.com>
2021-12-16 21:45:57 +0800

    move macros out of namespace

--- core/src/infrastructure/logger.h
+++ core/src/infrastructure/logger.h
@@ -3,14 +3,14 @@
 #include "spdlog/cfg/env.h"
 #include "spdlog/spdlog.h"
 
-namespace xpum {
-
 #define XPUM_LOG_INFO(...) spdlog::info(__VA_ARGS__)
 #define XPUM_LOG_WARN(...) spdlog::warn(__VA_ARGS__)
 #define XPUM_LOG_ERROR(...) spdlog::error(__VA_ARGS__)
 #define XPUM_LOG_DEBUG(...) spdlog::debug(__VA_ARGS__)
 #define XPUM_LOG_TRACE(...) spdlog::trace(__VA_ARGS__)
 #define XPUM_LOG_FATAL(...) spdlog::critical(__VA_ARGS__)
+
+namespace xpum {
 
 class Logger {
    public:
