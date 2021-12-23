b90e1aad9d2d830a0b562ed036c92bef1d11407d
yupengzh <yupeng.zhang@intel.com>
2021-12-22 22:03:54 -0800

    bug fix

--- cli/src/comlet_discovery.cpp
+++ cli/src/comlet_discovery.cpp
@@ -78,7 +78,7 @@ ComletDiscovery::ComletDiscovery() : ComletBase("discovery", "Discover the GPU d
 
 void ComletDiscovery::setupOptions() {
     this->opts = std::unique_ptr<ComletDiscoveryOptions>(new ComletDiscoveryOptions());
-    addOption("-d,--device", this->opts->deviceId, "device id");
+    addOption("-d,--device", this->opts->deviceId, "Device ID to query. It will show more detailed info.");
 }
 
 std::unique_ptr<nlohmann::json> ComletDiscovery::run() {
--- cli/src/comlet_dump.h
+++ cli/src/comlet_dump.h
@@ -28,6 +28,7 @@ struct MetricsOption {
     std::string key;
     std::string name;
     std::string description;
+    int scale = 1;
 };
 
 class ComletDump : public ComletBase {
@@ -36,14 +37,14 @@ class ComletDump : public ComletBase {
 
     std::vector<MetricsOption> metricsOptions{
         {XPUM_STATS_GPU_UTILIZATION, "XPUM_STATS_GPU_UTILIZATION", "GPU Utilization (%)", "GPU active time of the elapsed time, per tile"},
-        {XPUM_STATS_POWER, "XPUM_STATS_POWER", "GPU Power (W)", "per GPU"},
+        {XPUM_STATS_POWER, "XPUM_STATS_POWER", "GPU Power (W)", "per tile"},
         {XPUM_STATS_GPU_FREQUENCY, "XPUM_STATS_GPU_FREQUENCY", "GPU Frequency (MHz)", "per tile"},
         {XPUM_STATS_GPU_CORE_TEMPERATURE, "XPUM_STATS_GPU_CORE_TEMPERATURE", "GPU Core Temperature (Celsius Degree)", "per tile"},
         {XPUM_STATS_MEMORY_TEMPERATURE, "XPUM_STATS_MEMORY_TEMPERATURE", "GPU Memory Temperature (Celsius Degree)", "per tile"},
         {XPUM_STATS_MEMORY_UTILIZATION, "XPUM_STATS_MEMORY_UTILIZATION", "GPU Memory Utilization (%)", "per tile"},
         {XPUM_STATS_MEMORY_READ_THROUGHPUT, "XPUM_STATS_MEMORY_READ_THROUGHPUT", "GPU Memory Read (kB/s)", "per tile"},
         {XPUM_STATS_MEMORY_WRITE_THROUGHPUT, "XPUM_STATS_MEMORY_WRITE_THROUGHPUT", "GPU Memory Write (kB/s)", "per tile"},
-        {XPUM_STATS_ENERGY, "XPUM_STATS_ENERGY", "GPU Energy Consumed (J)", "per tile"},
+        {XPUM_STATS_ENERGY, "XPUM_STATS_ENERGY", "GPU Energy Consumed (J)", "per tile", 1000},
         {XPUM_STATS_EU_ACTIVE, "XPUM_STATS_EU_ACTIVE", "GPU EU Array Active (%)", "the normalized sum of all cycles on all EUs that were spent actively executing instructions. Per tile."},
         {XPUM_STATS_EU_STALL, "XPUM_STATS_EU_STALL", "GPU EU Array Stall (%)", "the normalized sum of all cycles on all EUs during which the EUs were stalled. Per tile.\n    At least one thread is loaded, but the EU is stalled. Per tile."},
         {XPUM_STATS_EU_IDLE, "XPUM_STATS_EU_IDLE", "GPU EU Array Idle (%)", "the normalized sum of all cycles on all cores when no threads were scheduled on a core. Per tile."},
@@ -53,7 +54,7 @@ class ComletDump : public ComletBase {
         {XPUM_STATS_RAS_ERROR_CAT_CACHE_ERRORS_CORRECTABLE, "XPUM_STATS_RAS_ERROR_CAT_CACHE_ERRORS_CORRECTABLE", "Cache Errors Correctable", "per tile."},
         {XPUM_STATS_RAS_ERROR_CAT_CACHE_ERRORS_UNCORRECTABLE, "XPUM_STATS_RAS_ERROR_CAT_CACHE_ERRORS_UNCORRECTABLE", "Cache Errors Uncorrectable", "per tile."},
         {XPUM_STATS_MEMORY_BANDWIDTH, "XPUM_STATS_MEMORY_BANDWIDTH", "GPU Memory Bandwidth Utilization (%)", ""},
-        {XPUM_STATS_MEMORY_USED, "XPUM_STATS_MEMORY_USED", "GPU Memory Used (MiB)", ""},
+        {XPUM_STATS_MEMORY_USED, "XPUM_STATS_MEMORY_USED", "GPU Memory Used (MiB)", "", 1024 * 1024},
     };
 
     std::string metricsHelpStr = "Metrics type to collect raw data, options. Separated by the comma.\n";
