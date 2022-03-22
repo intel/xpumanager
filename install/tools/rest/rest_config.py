fe5003f2433e5d62eff72bdd4c742379addac265
Wang, Yu W <yu.w.wang@intel.com>
2022-03-22 11:52:15 +0800

    add license header to more files

--- install/tools/rest/rest_config.py
+++ install/tools/rest/rest_config.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 #
 # Copyright (C) 2021-2022 Intel Corporation
 # SPDX-License-Identifier: MIT
 # @file rest_config.py
 #
 
-!/bin/env python3
 import configparser
 import os
 import pwd
