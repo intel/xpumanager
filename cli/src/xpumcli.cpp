36d00505cd923f83610c5ef776fea54641bf2a75
Zhang Wei <wei3.zhang@intel.com>
2022-03-22 15:00:52 +0800

    refactor code in cli for error message handling

--- cli/src/xpumcli.cpp
+++ cli/src/xpumcli.cpp
@@ -7,6 +7,12 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <grp.h>
+#include <unistd.h>
+#include <pwd.h>
 
 #include "CLI/App.hpp"
 #include "cli_wrapper.h"
@@ -30,6 +36,48 @@
 
 #define MAKE_COMLET_PTR(comlet_type) (std::static_pointer_cast<xpum::cli::ComletBase>(std::make_shared<comlet_type>()))
 
+bool permissionCheck() {
+    uid_t uid = getuid();
+	struct passwd* pw = getpwuid(uid);
+	if(pw == NULL){
+    	perror("getpwuid error");
+	}
+	int ngroups = 0;
+	getgrouplist(pw->pw_name, pw->pw_gid, NULL, &ngroups);
+    if (ngroups == 0) {
+        return false;
+    }
+	gid_t groups[ngroups];
+	getgrouplist(pw->pw_name, pw->pw_gid, groups, &ngroups);
+    std::string xpum_grp("xpum");
+    std::string root_grp("root");
+    bool has_permission = false;
+    for (int i = 0; i < ngroups; i++){
+	    struct group* gr = getgrgid(groups[i]);
+	    if(gr == NULL){
+	        perror("getgrgid error");
+	    }
+        std::string grp_name(gr->gr_name);
+		if (grp_name != xpum_grp && grp_name != root_grp) {
+            has_permission = true;
+        }
+	}
+    return has_permission;
+}
+
+bool serviceStatusCheck() {
+    FILE * f = popen("service xpum status", "r");
+    char c_line[1024];
+    while (fgets(c_line, 1024, f) != NULL) {
+        std::string line(c_line);
+        if (line.find("active (running)") != std::string::npos) {
+            return true;
+        }
+    }
+    pclose(f);
+    return false;
+}
+
 int main(int argc, char **argv) {
 
     xpum::cli::init_logger();
@@ -52,6 +100,11 @@ int main(int argc, char **argv) {
         .addComlet(MAKE_COMLET_PTR(xpum::cli::ComletDump))
         .addComlet(MAKE_COMLET_PTR(xpum::cli::ComletAgentSet));
     app.require_subcommand(0, 1);
+
+    if (!permissionCheck()) {
+        std::cout << "Error: User Permission Error\r\n";
+        return 0;
+    }
 
     if (argc == 1) {
         std::cout << app.help();
