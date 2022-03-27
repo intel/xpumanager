8ce5260776d01121bfd14f3f8a6bbce7a1815e6f
Wang, Yu W <yu.w.wang@intel.com>
2022-03-28 00:35:06 +0800

    fix build error due to import path issue

--- rest/views/exporter.py
+++ rest/views/exporter.py
@@ -5,7 +5,6 @@
 #
 
 from stub import devices
-from prometheus_exporter import get_metrics
 import stub
 
 import sys
@@ -14,6 +13,7 @@ import os
 rest_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.insert(1, os.path.join(rest_folder, "prometheus_exporter"))
+from prometheus_exporter import get_metrics # this import must follow sys.path.insert()
 
 
 def export_metrics():
