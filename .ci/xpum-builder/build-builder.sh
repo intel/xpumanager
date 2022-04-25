c5d2d321551f6d879df298995902774652e81cc8
Wang, Yu W <yu.w.wang@intel.com>
2022-04-25 22:35:12 +0800

    optimize builder images

--- .ci/xpum-builder/build-builder.sh
+++ .ci/xpum-builder/build-builder.sh
@@ -15,24 +15,35 @@ fi
 
 cd "$(dirname "$0")"
 
-if [ ! -d "grpc" ]
-then
-  git clone --depth 1 -b v1.45.0 https://github.com/grpc/grpc
-  pushd grpc
-  git submodule update --init
-  git -C third_party/zlib pull https://github.com/madler/zlib v1.2.12
-  git -C third_party/protobuf pull https://github.com/protocolbuffers/protobuf.git v3.19.4
-  popd
-else
-  echo "Using local grpc repo."
-fi
+proxy=http://child-prc.intel.com:913
 
-docker build --build-arg http_proxy=http://child-prc.intel.com:913 --build-arg https_proxy=http://child-prc.intel.com:913 -t sh1sdev002.sh.intel.com/xpum-builder-centos -t xpum-builder-centos -f Dockerfile.centos . > centos.log 2>&1 &
-docker build --build-arg http_proxy=http://child-prc.intel.com:913 --build-arg https_proxy=http://child-prc.intel.com:913 -t sh1sdev002.sh.intel.com/xpum-builder-ubuntu -t xpum-builder-ubuntu -f Dockerfile.ubuntu . > ubuntu.log 2>&1 &
+docker build \
+--build-arg http_proxy=$proxy \
+--build-arg https_proxy=$proxy \
+-t sh1sdev002.sh.intel.com/xpum-builder-centos \
+-t xpum-builder-centos -f Dockerfile.centos . > centos.log 2>&1 &
 
-docker build --build-arg http_proxy=http://child-prc.intel.com:913 --build-arg https_proxy=http://child-prc.intel.com:913 -t sh1sdev002.sh.intel.com/xpum-builder-centos7 -t xpum-builder-centos7 -f Dockerfile.centos7 . > centos7.log 2>&1 &
+docker build \
+--build-arg http_proxy=$proxy \
+--build-arg https_proxy=$proxy \
+-t sh1sdev002.sh.intel.com/xpum-builder-centos7 \
+-t xpum-builder-centos7 -f Dockerfile.centos7 . > centos7.log 2>&1 &
+
+docker build \
+--build-arg http_proxy=$proxy \
+--build-arg https_proxy=$proxy \
+-t sh1sdev002.sh.intel.com/xpum-builder-sles \
+-t xpum-builder-sles -f Dockerfile.sles . > sles.log 2>&1 &
+
+docker build \
+--build-arg http_proxy=$proxy \
+--build-arg https_proxy=$proxy \
+-t sh1sdev002.sh.intel.com/xpum-builder-ubuntu \
+-t xpum-builder-ubuntu -f Dockerfile.ubuntu . > ubuntu.log 2>&1 &
 
 wait
 
 docker push sh1sdev002.sh.intel.com/xpum-builder-centos
+docker push sh1sdev002.sh.intel.com/xpum-builder-centos7
 docker push sh1sdev002.sh.intel.com/xpum-builder-ubuntu
+docker push sh1sdev002.sh.intel.com/xpum-builder-sles
