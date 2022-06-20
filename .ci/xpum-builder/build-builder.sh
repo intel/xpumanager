c2bc41d89e7d2be83e62424bca076049e50cef3d
Wang, Yu W <yu.w.wang@intel.com>
2022-06-20 15:33:11 +0800

    build builder with lz-1.8.1

--- .ci/xpum-builder/build-builder.sh
+++ .ci/xpum-builder/build-builder.sh
@@ -8,6 +8,9 @@
 
 set -ex
 
+level_zero_version=1.8.1
+image_tag=lz-$level_zero_version
+
 if [ "$EUID" -ne 0 ]
   then echo "please run as root"
   exit
@@ -20,37 +23,41 @@ proxy=http://child-prc.intel.com:913
 docker build \
 --build-arg http_proxy=$proxy \
 --build-arg https_proxy=$proxy \
--t sh1sdev002.sh.intel.com/xpum-builder-centos \
+--build-arg level_zero_version=$level_zero_version \
+-t sh1sdev002.sh.intel.com/xpum-builder-centos:$image_tag \
 -f Dockerfile.centos . > centos.log 2>&1 &
 
 docker build \
 --build-arg http_proxy=$proxy \
 --build-arg https_proxy=$proxy \
--t sh1sdev002.sh.intel.com/xpum-builder-centos7 \
+--build-arg level_zero_version=$level_zero_version \
+-t sh1sdev002.sh.intel.com/xpum-builder-centos7:$image_tag \
 -f Dockerfile.centos7 . > centos7.log 2>&1 &
 
 docker build \
 --build-arg http_proxy=$proxy \
 --build-arg https_proxy=$proxy \
--t sh1sdev002.sh.intel.com/xpum-builder-sles \
+--build-arg level_zero_version=$level_zero_version \
+-t sh1sdev002.sh.intel.com/xpum-builder-sles:$image_tag \
 -f Dockerfile.sles . > sles.log 2>&1 &
 
 docker build \
 --build-arg http_proxy=$proxy \
 --build-arg https_proxy=$proxy \
--t sh1sdev002.sh.intel.com/xpum-builder-ubuntu \
+--build-arg level_zero_version=$level_zero_version \
+-t sh1sdev002.sh.intel.com/xpum-builder-ubuntu:$image_tag \
 -f Dockerfile.ubuntu . > ubuntu.log 2>&1 &
 
 wait
 
-docker push sh1sdev002.sh.intel.com/xpum-builder-centos
-docker push sh1sdev002.sh.intel.com/xpum-builder-centos7
-docker push sh1sdev002.sh.intel.com/xpum-builder-ubuntu
-docker push sh1sdev002.sh.intel.com/xpum-builder-sles
+docker push sh1sdev002.sh.intel.com/xpum-builder-centos:$image_tag
+docker push sh1sdev002.sh.intel.com/xpum-builder-centos7:$image_tag
+docker push sh1sdev002.sh.intel.com/xpum-builder-ubuntu:$image_tag
+docker push sh1sdev002.sh.intel.com/xpum-builder-sles:$image_tag
 
-docker pull sh1sdev002.sh.intel.com/xpum-builder-centos
-docker pull sh1sdev002.sh.intel.com/xpum-builder-centos7
-docker pull sh1sdev002.sh.intel.com/xpum-builder-ubuntu
-docker pull sh1sdev002.sh.intel.com/xpum-builder-sles
+docker pull sh1sdev002.sh.intel.com/xpum-builder-centos:$image_tag
+docker pull sh1sdev002.sh.intel.com/xpum-builder-centos7:$image_tag
+docker pull sh1sdev002.sh.intel.com/xpum-builder-ubuntu:$image_tag
+docker pull sh1sdev002.sh.intel.com/xpum-builder-sles:$image_tag
 
 docker system prune
