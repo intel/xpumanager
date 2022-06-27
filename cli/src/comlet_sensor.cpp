5d724fd6615beaeb1866860bc61cde8124120b0d
yupengzh <yupeng.zhang@intel.com>
2022-06-26 19:23:44 -0700

    refine sensor reading table view

--- cli/src/comlet_sensor.cpp
+++ cli/src/comlet_sensor.cpp
@@ -7,33 +7,40 @@
 #include "comlet_sensor.h"
 #include "cli_table.h"
 
+using namespace nlohmann;
+
 namespace xpum::cli {
 
 static CharTableConfig ComletSensorTableConfig(R"({
-    "columns": [{
-        "title": "AMC Index"
-    },{
-        "title": "Sensor Name"
-    },{
-        "title": "Value"
-    },{
-        "title": "Low"
-    },{
-        "title": "High"
-    },{
-        "title": "Unit"
-    }],
-    "rows": [{
-        "instance": "sensor_reading[]",
-        "cells": [
-            { "value": "amc_index" },
-            "sensor_name", 
-            "value", 
-            "sensor_low",
-            "sensor_high",
-            "sensor_unit"
-        ]
-    }]
+    "columns": [
+        {
+            "title": "Sensor Name"
+        },
+        {
+            "title": "Value"
+        },
+        {
+            "title": "Low"
+        },
+        {
+            "title": "High"
+        },
+        {
+            "title": "Unit"
+        }
+    ],
+    "rows": [
+        {
+            "instance": "sensor_reading[]",
+            "cells": [
+                "sensor_name",
+                "value",
+                "sensor_low",
+                "sensor_high",
+                "sensor_unit"
+            ]
+        }
+    ]
 })"_json);
 
 std::unique_ptr<nlohmann::json> ComletSensor::run(){
@@ -42,7 +49,22 @@ std::unique_ptr<nlohmann::json> ComletSensor::run(){
 
 void ComletSensor::getTableResult(std::ostream &out) {
     auto p_json = run();
-    CharTable table(ComletSensorTableConfig, *p_json);
-    table.show(out);
+    json sensorJson = (*p_json);
+    std::map<int, json> m;
+    for (auto obj : sensorJson["sensor_reading"]) {
+        int amc_index = obj["amc_index"];
+        m[amc_index].push_back(obj);
+    }
+    for (auto it = m.begin(); it != m.end(); it++) {
+        int amc_index = it->first;
+        auto sensor_reading = it->second;
+        json obj;
+        obj["amc_index"] = amc_index;
+        obj["sensor_reading"] = sensor_reading;
+
+        out << "AMC " << amc_index << std::endl;
+        CharTable table(ComletSensorTableConfig, obj);
+        table.show(out);
+    }
 }
 }
