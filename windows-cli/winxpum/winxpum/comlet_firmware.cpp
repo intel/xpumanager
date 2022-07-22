7aaad5e7ab31a098019436093a08baabea85154e
fmiao <fabiao.miao@intel.com>
2022-07-22 09:03:55 +0800

    remove Reboot Message after GSC Update

--- windows-cli/winxpum/winxpum/comlet_firmware.cpp
+++ windows-cli/winxpum/winxpum/comlet_firmware.cpp
@@ -371,7 +371,6 @@ void ComletFirmware::getTableResult(std::ostream &out) {
         if (flashStatus.compare("OK") == 0) {
             out << std::endl;
             out << "Update firmware successfully." << std::endl;
-            out << "Please reboot OS to take effect." << std::endl;
             return;
         } else if (flashStatus.compare("FAILED") == 0) {
             out << std::endl;
