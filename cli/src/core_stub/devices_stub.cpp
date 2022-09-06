843079db7b52aa5565de1152140cd44c17450da8
fmiao <fabiao.miao@intel.com>
2022-09-06 09:58:27 +0800

    fix bug

--- cli/src/core_stub/devices_stub.cpp
+++ cli/src/core_stub/devices_stub.cpp
@@ -32,6 +32,7 @@ std::unique_ptr<nlohmann::json> LibCoreStub::getDeviceList() {
             deviceJson["pci_device_id"] = devices[i].PCIDeviceId;
             deviceJson["pci_bdf_address"] = devices[i].PCIBDFAddress;
             deviceJson["vendor_name"] = devices[i].VendorName;
+            deviceJson["drm_device"] = devices[i].drmDevice;
             deviceJsonList.push_back(deviceJson);
         }
         (*json)["device_list"] = deviceJsonList;
