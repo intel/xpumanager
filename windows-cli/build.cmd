4287f918b5f01d5618c3699aa40d6bf9151f0c64
Wang, Yu W <yu.w.wang@intel.com>
2022-11-16 14:32:55 +0800

    fix windows build script
    tfs:40046d0b4bc522401631ec7d9068556a7146b1d2

--- windows-cli/build.cmd
+++ windows-cli/build.cmd
@@ -1,9 +1,9 @@
-@echo off
+@echo on
 SETLOCAL
 
 SET REV=%1
 
-SET WORK_DIR="%~dp0"
+SET WORK_DIR=%~dp0
 cd %WORK_DIR%
 powershell -Command "(Get-Content winxpum\winxpum\winxpum.rc) -replace '\"ProductVersion\".*', '\"ProductVersion\", \"%REV%\"' | Out-File -encoding ASCII winxpum\winxpum\winxpum.rc"
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" winxpum\winxpum.sln /Rebuild "release|x64"
