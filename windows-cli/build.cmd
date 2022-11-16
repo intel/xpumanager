84c9b9bfc2a19861f8555d44ac132fa1be71620f
Wang, Yu W <yu.w.wang@intel.com>
2022-11-16 14:22:43 +0800

    update windows cli build scripts for signfile
    tfs:2c2d46b2ca22c575cbd37315fafc2f82be3f969c

--- windows-cli/build.cmd
+++ windows-cli/build.cmd
@@ -3,7 +3,10 @@ SETLOCAL
 
 SET REV=%1
 
-cd "%~dp0"
+SET WORK_DIR="%~dp0"
+cd %WORK_DIR%
 powershell -Command "(Get-Content winxpum\winxpum\winxpum.rc) -replace '\"ProductVersion\".*', '\"ProductVersion\", \"%REV%\"' | Out-File -encoding ASCII winxpum\winxpum\winxpum.rc"
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" winxpum\winxpum.sln /Rebuild "release|x64"
-..\install\tools\signfile\SignFile.exe -u %SIGN_USER% -p %SIGN_PASS% -conf ..\install\tools\signfile\SignFileWin.config.xml winxpum\Release\xpumcli.exe
+pushd ..\install\tools\signfile
+SignFile.exe -u %SIGN_USER% -p %SIGN_PASS% -conf SignFileWin.config.xml %WORK_DIR%\winxpum\Release\xpumcli.exe
+popd
