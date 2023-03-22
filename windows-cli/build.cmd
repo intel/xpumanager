75ec8d6638d5e95c246a4bc62c2e207ec7db51f4
Huiqi Wang <huiqi.wang@intel.com>
2023-03-22 13:15:50 +0800

    [XM7-284]fix bug of winxpum version (#224)

--- windows-cli/build.cmd
+++ windows-cli/build.cmd
@@ -5,7 +5,7 @@ SET REV=%1
 
 SET WORK_DIR=%~dp0
 cd %WORK_DIR%
-powershell -Command "(Get-Content winxpum\winxpum\winxpum.rc) -replace '\"ProductVersion\".*', '\"ProductVersion\", \"%REV%\"' | Out-File -encoding ASCII winxpum\winxpum\winxpum.rc"
+powershell -Command "(Get-Content winxpum\winxpum\resource.h) -replace 'VER_COMMIT_VERSION VER_VERSION_MAJORMINORPATCH_STR', 'VER_COMMIT_VERSION_TEMP \"%REV%\"' | Out-File  -encoding ASCII winxpum\winxpum\resource.h"
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" winxpum\winxpum.sln /Rebuild "release|x64"
 pushd ..\install\tools\signfile
 SignFile.exe -u %SIGN_USER% -p %SIGN_PASS% -conf SignFileWin.config.xml %WORK_DIR%\winxpum\Release\xpumcli.exe
