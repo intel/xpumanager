d4a2dde06d8961f244d53101b0e2ec0a59fb74aa
Wang, Zheng W <zheng.w.wang@intel.com>
2023-05-12 16:30:11 +0800

    [XM7-365] Fix it by redirecting help message when -PP is not supported by lspci (#311)

--- cli/src/local_functions.cpp
+++ cli/src/local_functions.cpp
@@ -339,7 +339,7 @@ bool getPciPath(std::vector<string> &pciPath, const std::string &bdf) {
     //An example of the output of the cmd belew is:
     //0000:4a:02.0/4b:00.0/4c:01.0/4d:00.0
     //And the function would return a vector (pciPath) of full BDF
-    string cmd = "lspci -DPPs " + bdf + "|cut -d ' ' -f 1";
+    string cmd = "lspci -DPPs " + bdf + " 2>&1|cut -d ' ' -f 1";
     char path[PATH_MAX];
     FILE *pf = popen(cmd.c_str(), "r");
     if (pf == NULL) {
