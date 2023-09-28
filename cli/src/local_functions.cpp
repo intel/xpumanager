c3d62475c44a307b7a9ef638c1b46937617f367f
Wang, Zheng W <zheng.w.wang@intel.com>
2023-09-28 14:49:18 +0800

    [XM7-590] Hard code default power limit (0) for ATS-M CLI (#611)

--- cli/src/local_functions.cpp
+++ cli/src/local_functions.cpp
@@ -328,6 +328,16 @@ bool isPhysicalFunctionDevice(std::string pci_addr) {
     return true;
 }
 
+bool isATSM3(std::string &pciId) {
+    std::transform(pciId.begin(), pciId.end(), pciId.begin(), ::tolower);
+    return pciId.find("56c1") != std::string::npos;
+}
+
+bool isATSM1(std::string &pciId) {
+    std::transform(pciId.begin(), pciId.end(), pciId.begin(), ::tolower);
+    return pciId.find("56c0") != std::string::npos;
+}
+
 bool isATSMPlatform(std::string str) {
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
     return str.find("56c0") != std::string::npos || str.find("56c1") != std::string::npos;
