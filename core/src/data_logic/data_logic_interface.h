8c432385f61f8934fd80e0cdb16c9c794d952abc
Wang, Zheng W <zheng.w.wang@intel.com>
2024-01-08 11:15:21 +0800

    [XM7-672] Remove two useless interfaces (#693)

--- core/src/data_logic/data_logic_interface.h
+++ core/src/data_logic/data_logic_interface.h
@@ -6,17 +6,71 @@
 
 #pragma once
 
-#include "data_logic_persistence_interface.h"
-#include "data_logic_query_interface.h"
+#include <map>
+#include <deque>
+
+#include "infrastructure/const.h"
+#include "infrastructure/measurement_data.h"
+#include "infrastructure/measurement_cache_data.h"
+#include "infrastructure/measurement_type.h"
 #include "infrastructure/init_close_interface.h"
+#include "../include/xpum_structs.h"
+#include "api/internal_api_structs.h"
 
 namespace xpum {
 
-class DataLogicInterface : public InitCloseInterface,
-                           public DataLogicQueryInterface,
-                           public DataLogicPersistenceInterface {
-   public:
-    virtual ~DataLogicInterface(){};
+class DataLogicInterface : public InitCloseInterface {
+    public:
+        virtual ~DataLogicInterface(){};
+        virtual void storeMeasurementData(
+                MeasurementType type,
+                Timestamp_t time,
+                std::shared_ptr<std::map<std::string, std::shared_ptr<MeasurementData>>> datas) = 0;
+        virtual std::shared_ptr<MeasurementData> getLatestData(MeasurementType type,
+                std::string &device_id) = 0;
+        virtual void getLatestData(MeasurementType type,
+                std::map<std::string, std::shared_ptr<MeasurementData>> &datas) = 0;
+        virtual std::shared_ptr<MeasurementData> getLatestStatistics(MeasurementType type, std::string &device_id, uint64_t session_id) = 0;
+        virtual xpum_result_t getMetricsStatistics(xpum_device_id_t deviceId,
+                xpum_device_stats_t dataList[],
+                uint32_t *count,
+                uint64_t *begin,
+                uint64_t *end,
+                uint64_t session_id) = 0;
+        virtual xpum_result_t getEngineStatistics(xpum_device_id_t deviceId,
+                xpum_device_engine_stats_t dataList[],
+                uint32_t *count,
+                uint64_t *begin,
+                uint64_t *end,
+                uint64_t session_id) = 0;
+        virtual void getLatestMetrics(xpum_device_id_t deviceId,
+                xpum_device_metrics_t dataList[],
+                int *count) = 0;
+        virtual xpum_result_t getEngineUtilizations(xpum_device_id_t deviceId,
+                xpum_device_engine_metric_t dataList[],
+                uint32_t *count) = 0;
+        virtual xpum_result_t getFabricThroughputStatistics(xpum_device_id_t deviceId,
+                xpum_device_fabric_throughput_stats_t dataList[],
+                uint32_t *count,
+                uint64_t *begin,
+                uint64_t *end,
+                uint64_t session_id) = 0;
+        virtual xpum_result_t getFabricThroughput(xpum_device_id_t deviceId,
+                xpum_device_fabric_throughput_metric_t dataList[],
+                uint32_t *count) = 0;
+        virtual bool getFabricLinkInfo(xpum_device_id_t deviceId,
+                FabricLinkInfo info[],
+                uint32_t *count) = 0;
+        virtual uint32_t startRawDataCollectionTask(xpum_device_id_t device_id, std::vector<MeasurementType> types) = 0;
+        virtual void stopRawDataCollectionTask(uint32_t task_id) = 0;
+        virtual std::deque<MeasurementCacheData> getCachedRawData(uint32_t task_id, MeasurementType type) = 0;
+        virtual std::vector<std::deque<MeasurementCacheData>> getCachedRawData(uint32_t task_id) = 0;
+        virtual void updateStatsTimestamp(uint32_t session_id, uint32_t device_id) = 0;
+        virtual uint64_t getStatsTimestamp(uint32_t session_id, uint32_t device_id) = 0;
+        virtual void updateEngineStatsTimestamp(uint32_t session_id, uint32_t device_id) = 0;
+        virtual uint64_t getEngineStatsTimestamp(uint32_t session_id, uint32_t device_id) = 0;
+        virtual void updateFabricStatsTimestamp(uint32_t session_id, uint32_t device_id) = 0;
+        virtual uint64_t getFabricStatsTimestamp(uint32_t session_id, uint32_t device_id) = 0;
 };
 
 } // end namespace xpum
