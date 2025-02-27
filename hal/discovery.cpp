47f71694dea860bcb7b293a94186a875b24fad37
Satyeshwar Singh <satyeshwar.singh@intel.com>
2025-02-27 12:01:21 -0800

    hal: Added discovery class
    It currently doesn't have any content in it but needed it to test out
    how IAL can access multiple classes.
    
    Signed-off-by: Satyeshwar Singh <satyeshwar.singh@intel.com>

---
+++ hal/discovery.cpp
@@ -0,0 +1,15 @@
+#include <discovery.h>
+#include <iostream>
+
+using namespace std;
+
+void discovery::help(help_cmd *help_msg, int *lines_filled)
+{
+	cout<<"Discovery help"<<endl;
+}
+
+int discovery::run()
+{
+	cout<<"Discovery run"<<endl;
+	return 0;
+}
