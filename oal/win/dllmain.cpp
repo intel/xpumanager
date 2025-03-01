35c5f7150ba6520cca8303da4a99b3df04806608
Satyeshwar Singh <satyeshwar.singh@intel.com>
2025-02-28 16:56:07 -0800

    Win: Increased warning level to 4 to catch more issues
    As a result, two unused parameter warnings popped up in dllmain.cpp.
    Fixed those as well.
    
    Signed-off-by: Satyeshwar Singh <satyeshwar.singh@intel.com>

--- oal/win/dllmain.cpp
+++ oal/win/dllmain.cpp
@@ -1,18 +1,21 @@
 #include <os.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	UNUSED(lpReserved);
+	UNUSED(hModule);
+
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
