393e052303d8f6ca727640c640a82e36bfe80294
Satyeshwar Singh <satyeshwar.singh@intel.com>
2025-03-04 16:24:43 -0800

    lz: Added device info in p_dev
    Moved device info into p_dev rather than lz class. This way, it sits
    with PCI and MMIO data structures on a per device basis. Also, cleaned
    up any allocated lz memory.
    
    Signed-off-by: Satyeshwar Singh <satyeshwar.singh@intel.com>

--- hal/lz.h
+++ hal/lz.h
@@ -2,8 +2,6 @@
 #define _LZ_H
 
 #include <string>
-#include <ze_api.h>
-#include <zet_api.h>
 #include <list>
 #include <vector>
 #include "pdev.h"
@@ -13,19 +11,17 @@ using namespace std;
 class lz {
 protected:
 	ze_driver_handle_t p_driver;
-	ze_device_handle_t p_device[MAX_DEVS];
-	int total_devs;
 	ze_context_handle_t context;
-	ze_device_properties_t device_properties;
 	bool init;
 
 	bool init_ze();
 	bool find_dev(ze_driver_handle_t p_dr,
-		ze_device_type_t type);
+		ze_device_type_t type, p_dev *devs, int found_devs);
 public:
 	lz() { init = false; }
-	~lz() { };
-	ze_result_t initialize();
+	~lz();
+	ze_result_t initialize(p_dev *devs, int found_devs);
+	void cleanup(p_dev *devs, int found_devs);
 	void print_loader_versions();
 	bool is_init() { return init; }
 };
