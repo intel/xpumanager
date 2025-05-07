7ee789554e6a5507305e574efa8242ad352d7a2d
Singh, Satyeshwar <satyeshwar.singh@intel.com>
2025-05-07 13:18:50 -0700

    fwupd: Added a hal/fwupd folder for all files related to firmware update
    This folder will have different ways to do firmware update. For example,
    gsc, sysman, AMC etc. For now,it just has shell functions.
    
    Signed-off-by: Singh, Satyeshwar <satyeshwar.singh@intel.com>

---
+++ hal/fwupd/fwupd.h
@@ -0,0 +1,11 @@
+#ifndef _FWUPD_H_
+#define _FWUPD_H_
+
+class fwupd
+{
+
+public:
+	virtual void updateFirmware() = 0;
+};
+
+#endif
---
+++ hal/fwupd/gscupd.cpp
@@ -0,0 +1,8 @@
+#include "gscupd.h"
+#include <debug.h>
+
+void gscupd::updateFirmware()
+{
+	TRACING();
+	DBG("Updating firmware...\n");
+}
