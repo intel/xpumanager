a0452044a2f3978962459d895f3d7569d97819f8
Singh, Satyeshwar <satyeshwar.singh@intel.com>
2025-09-16 11:06:42 -0700

    global_lock: Added a cross-process lock while updating firmware
    If we are updating the firmware through one instance of XPUM, we can't
    allow another instance (process) to try doing another update. This is
    due to the fact that for certain firmwares like AMC, XPUM has to
    initialize the i2c child device driver and this can interfere with an
    ongoing firmware update.
    In order to prevent another instance from trying to do a firmware
    update, we had to create Windows and Linux based files whose presence
    will be checked by each XPUM instance before starting any firmware
    updates.
    
    This is part of a fix for XPUM-945.
    
    Signed-off-by: Singh, Satyeshwar <satyeshwar.singh@intel.com>

---
+++ oal/win/fs_lock.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright © 2025 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+#include <fs_lock.h>
+#include <os.h>
+#include <windows.h>
+
+/*
+ * @brief 	On Windows, we use CreateFile with no sharing to create an exclusive lock.
+ * The lock file is placed in C:/ProgramData; ensure the directory exists externally if needed.
+ * The lock is released by closing the handle and deleting the file.
+ */
+void FSLock::acquire()
+{
+	// Use a file in ProgramData; ensure directory exists externally if needed.
+	const char *lockPath = "C:/ProgramData/xpum_firmware_update.lock";
+	HANDLE hFile =
+		CreateFileA(lockPath, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		acquired = false;
+		handle = 0;
+		return;
+	}
+	acquired = true;							 // Exclusive because share mode = 0
+	lockFilePath = lockPath;					 // Store the path for cleanup
+	handle = reinterpret_cast<uintptr_t>(hFile); // Store HANDLE as uintptr_t
+}
+
+/*
+ * @brief 	Release the exclusive lock by closing the handle and deleting the lock file.
+ */
+void FSLock::release()
+{
+	HANDLE hFile = reinterpret_cast<HANDLE>(handle);
+	if (acquired && handle != 0 && hFile != INVALID_HANDLE_VALUE) {
+		CloseHandle(hFile);
+		handle = 0;
+		acquired = false;
+
+		// Delete the lock file
+		if (!lockFilePath.empty()) {
+			DeleteFileA(lockFilePath.c_str());
+		}
+	}
+}
