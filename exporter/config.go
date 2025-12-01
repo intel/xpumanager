e387af7e9352eb3a3b635fc70608813e11cf411a
Markus Lehtonen <markus.lehtonen@intel.com>
2025-12-01 16:21:05 +0200

    Add configuration for the exporter
    Add configuration options for the exporter. Update example config.

--- exporter/config.go
+++ exporter/config.go
@@ -5,18 +5,35 @@
 
 package exporter
 
-import "go.opentelemetry.io/collector/component"
+import (
+	"fmt"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/confignet"
+)
 
 // Config defines configuration for the exporter.
 type Config struct {
+	configgrpc.ServerConfig `mapstructure:",squash"`
 }
 
 // Validate checks if the configuration is valid.
 func (cfg *Config) Validate() error {
+	if cfg.NetAddr.Transport != confignet.TransportTypeUnix {
+		return fmt.Errorf("unsupported transport type: %q, only %q is supported", cfg.NetAddr.Transport, confignet.TransportTypeUnix)
+	}
 	return nil
 }
 
 // defaultConfig provides default configuration values.
 func defaultConfig() component.Config {
-	return &Config{}
+	cfg := &Config{
+		ServerConfig: configgrpc.NewDefaultServerConfig(),
+	}
+
+	// Set transport to Unix Domain Sockets by default.
+	cfg.NetAddr.Transport = confignet.TransportTypeUnix
+
+	return cfg
 }
