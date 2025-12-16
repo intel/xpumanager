6881edb2eedb2c88dbe3ff073303482574d40bec
Markus Lehtonen <markus.lehtonen@intel.com>
2025-12-16 08:22:28 +0200

    core: add Ze API version check
    Add a compile-time check that the Level Zero API version of the used C headers
    is at least the version that was used to generate the bindings.

--- core/core_static.go
+++ core/core_static.go
@@ -9,6 +9,21 @@ package core
 
 import "errors"
 
+/*
+#cgo pkg-config: level-zero
+#include "ze_api.h"
+#include <stdlib.h>
+#include "cgo_helpers.h"
+*/
+import "C"
+
+// Check that the API version of the C headers used for compilation is at least
+// the version that was used to generate the bindings.
+// Will fail with "invalid array length" compiler error if the
+// ZE_API_VERSION_CURRENT from the C headers is less than API_VERSION_CURRENT
+// from the bindings.
+var _ [C.ZE_API_VERSION_CURRENT - API_VERSION_CURRENT]struct{}
+
 // ToError converts the Result to an error.
 func (r *Result) ToError() error {
 	if *r == RESULT_SUCCESS {
