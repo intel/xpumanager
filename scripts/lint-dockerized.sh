8cec366ea67ef7d7cd78cb77b90af50b8c4d8ca7
Markus Lehtonen <markus.lehtonen@intel.com>
2025-12-17 11:04:46 +0200

    ci: install up-to-date version of libze for golangci-lint
    Put the script in scripts/lint-dockerized.sh for easier local testing.

---
+++ scripts/lint-dockerized.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+LEVEL_ZERO_VERSION=${LEVEL_ZERO_VERSION:-"1.26.0"}
+L0_BASE_URL="https://github.com/oneapi-src/level-zero/releases/download"
+
+docker run --rm -v $(pwd):/app -w /app \
+    golangci/golangci-lint:latest \
+    /bin/sh -c \
+    "curl -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb \
+          -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero-devel_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb && \
+     apt-get install -y ./*.deb && \
+     golangci-lint run --color=always"
