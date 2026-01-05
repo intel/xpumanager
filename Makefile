intel/xpumanager

commit 9a1c6748c2d2cf5f10b9dabe4f2ada0045902edf

Fix "make stats" output for long extensions
And make "help" target matching more robust.

Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

Makefile
@@ -9,15 +9,15 @@ OUT_DIR ?= bin
 
 .PHONY: help
 help:
-	@echo -e "\nTargets:\n$$(grep '^[^:. ]*:' Makefile | sed -e 's/^/- /' -e 's/:$$//')\n"
+	@echo -e "\nTargets:\n$$(grep '^[#^:. ]*:' Makefile | sed -e 's/^/- /' -e 's/:$$//')\n"
 
 .PHONY: stats
 stats:
-	@echo "Type:  Files:   Lines:"
+	@echo "Type:       Files:   Lines:"
 	@for ext in $$(git ls-files | sed -e 's%.*/%%' -e 's/.*\././' | grep -F . | sort -u); do \
 		files=$$(git ls-files "*$$ext" | wc -l); \
 		lines=$$(git ls-files -z "*$$ext" | xargs -0 xargs cat | wc -l); \
-		printf " %-5s %5d %8d\n" $$ext $$files $$lines; \
+		printf " %-10s %5d %8d\n" $$ext $$files $$lines; \
 	done
 
 # --- Build targets ---

commit 8c91f85194c1d5671346f880322dccda3ce7bba2

Fix "make help"
Fixes: 9a1c6748c2d2cf5f10

Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

Makefile
@@ -9,7 +9,7 @@ OUT_DIR ?= bin
 
 .PHONY: help
 help:
-	@echo -e "\nTargets:\n$$(grep '^[#^:. ]*:' Makefile | sed -e 's/^/- /' -e 's/:$$//')\n"
+	@echo -e "\nTargets:\n$$(grep '^[^#:. ]*:' Makefile | sed -e 's/^/- /' -e 's/:$$//')\n"
 
 .PHONY: stats
 stats: