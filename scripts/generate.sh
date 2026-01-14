6af8302ef3aa57000a86ac6bbb982f49e7cb1c84
Markus Lehtonen <markus.lehtonen@intel.com>
2026-01-14 14:02:49 +0200

    receiver/sysman: use mdatagen for managing resource boilerplate
    Add metadata.yaml for the sysman scraper. Documents all metrics and
    their attributes.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,7 +3,7 @@
 # We assume this script is run from the root of the repository
 ROOT_DIR=$(pwd)
 
-go -C receiver generate
+go -C receiver generate ./...
 
 go -C charts/xpumd tool -modfile "$ROOT_DIR/tools/go.mod" helm-docs
 go -C charts/xpumd tool -modfile "$ROOT_DIR/tools/go.mod" helm-values-schema-json
