6b41fc6946d8d53c394f15b6bb20b42bd1aac8b3
Shan, Michael1 <michael1.shan@intel.com>
2026-01-16 11:12:35 -0800

    cmd_diag: added JSON supports for "xpu-smi diag" command
    At present, the xpu-smi CLI doesn't support --json option in the diag command.
    This commit implements JSON feature for all diag commands, except stress options,
    which covers:
          1). xpu-smi diag -d [deviceId] -l [level] -j
          2). xpu-smi diag -d [deviceId] --singletest [testIds] -j
          3). xpu-smi diag --precheck -j
          4). xpu-smi diag --precheck --listtypes -j
          5). xpu-smi diag --precheck --gpu -j
    
    JIRA: \1
    
    Signed-off-by: Shan, Michael <michael1.shan@intel.com>

--- ial/cmn/cmd_diag.h
+++ ial/cmn/cmd_diag.h
@@ -8,6 +8,7 @@
 #define _CMD_DIAG_H
 
 #include "cmds.h"
+#include "printer.h"
 #include <os.h>
 #include <string>
 
@@ -68,7 +69,7 @@ enum diagLevel
 
 class cmdDiag;
 
-using diagSubCmdFunc = ze_result_t (cmdDiag::*)(devInfo *d);
+using diagSubCmdFunc = ze_result_t (cmdDiag::*)(devInfo *d, nlohmann::ordered_json *cmdJson);
 struct diagCmdStruct
 {
 	option opt;
@@ -89,6 +90,16 @@ struct diagSubSingleCmdStruct
 	diagSubCmdFunc func;
 };
 
+/**
+ * @brief Discovery-specific text printer that formats discovery output as human-readable text
+ */
+class DiagTextPrinter : public Printer
+{
+public:
+	DiagTextPrinter() : Printer() {}
+	void print(nlohmann::ordered_json *jsonObj) override;
+};
+
 class cmdDiag : public cmds
 {
 private:
@@ -102,28 +113,36 @@ public:
 	cmdDiag() : driverLoaded(false), sysInfoShown(false) { STRCPY_S(name, MAX_PATH, "diag"); };
 	~cmdDiag(){};
 	void help(HELP helpType = FULL_HELP);
-	ze_result_t precheck(devInfo *d);
-	ze_result_t stress(devInfo *d);
-	ze_result_t level(devInfo *d);
-	ze_result_t runSingleTest(devInfo *d);
-	ze_result_t listTypes(devInfo *d);
-	ze_result_t gpu(devInfo *d);
+	ze_result_t precheck(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t stress(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t level(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t runSingleTest(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t listTypes(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t gpu(devInfo *d, nlohmann::ordered_json *jsonObj);
 	ze_result_t printPrecheckInfo(devInfo *d, bool gpuOnly);
-	ze_result_t runSince(devInfo *d);
-	ze_result_t checkLibrary(devInfo *d);
-	ze_result_t checkExclusive(devInfo *d);
-	ze_result_t checkAccessPermission(devInfo *d);
-	ze_result_t computation(devInfo *d);
-	ze_result_t memoryError(devInfo *d);
-	ze_result_t memoryAllocation(devInfo *d);
-	ze_result_t memoryBandwidth(devInfo *d);
-	ze_result_t mediaCodec(devInfo *d);
-	ze_result_t pcieBandwidth(devInfo *d);
-	ze_result_t powerTest(devInfo *d);
-	ze_result_t computationFuncTest(devInfo *d);
-	ze_result_t mediaFuncTest(devInfo *d);
-	ze_result_t xeLinkThroughput(devInfo *d);
-	ze_result_t xeLinkAllToAllThroughput(devInfo *d);
+	ze_result_t runSince(devInfo *d, nlohmann::ordered_json *jsonObj);
+	std::unique_ptr<nlohmann::ordered_json> printDiagDetail(std::string componentType, std::string componentMsg,
+															std::string componentResult);
+	void printSingleDiagInfo(devInfo *d, nlohmann::ordered_json *jsonObj, std::string type, std::string msg,
+							 std::string result);
+	std::unique_ptr<nlohmann::ordered_json> printErrorDetail(std::string errCategory, std::string errID,
+															 std::string errSeverity, std::string errType);
+	void printErrorInfo(nlohmann::ordered_json *errListJson, std::string errCategory, std::string errID,
+						std::string errSeverity, std::string errType);
+	ze_result_t checkLibrary(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t checkExclusive(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t checkAccessPermission(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t computation(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t memoryError(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t memoryAllocation(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t memoryBandwidth(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t mediaCodec(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t pcieBandwidth(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t powerTest(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t computationFuncTest(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t mediaFuncTest(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t xeLinkThroughput(devInfo *d, nlohmann::ordered_json *jsonObj);
+	ze_result_t xeLinkAllToAllThroughput(devInfo *d, nlohmann::ordered_json *jsonObj);
 
 	int run(arg_struct *args);
 };
