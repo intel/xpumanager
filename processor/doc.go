374c1901c0ad2752bd090238ce0e1da100703be8
Markus Lehtonen <markus.lehtonen@intel.com>
2026-01-21 20:31:03 +0200

    Add processor module
    Add metadata for custom status metric processor.

---
+++ processor/doc.go
@@ -0,0 +1,8 @@
+//
+// Copyright (C) 2026 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package processor
+
+//go:generate go tool -modfile ../tools/go.mod mdatagen metadata.yaml
---
+++ processor/go.mod
@@ -0,0 +1,3 @@
+module github.com/intel/xpumanager/processor
+
+go 1.25.0
