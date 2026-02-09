117e6cee583abb890b7801446e09d7e221e768b5
Shan, Michael1 <michael1.shan@intel.com>
2026-02-09 14:23:57 -0800

    Use the const instead of #define Macro
    This commit uses consts instead of #define Macros, that should be easy to read and less
    error prone.
    
    JIRA: \1
    
    Signed-off-by: Shan, Michael <michael1.shan@intel.com>

--- ial/cli/cli.h
+++ ial/cli/cli.h
@@ -10,12 +10,6 @@
 #include <cmds.h>
 #include <functional>
 
-#define STRINGIFY(x) #x
-#define CONCATENATE_WITH_DOT(a, b) "v" STRINGIFY(a) "." STRINGIFY(b)
-#define CONCATENATE_WITH_DOTS(a, b, c, d) STRINGIFY(a) "." STRINGIFY(b) "." STRINGIFY(c) "." STRINGIFY(d)
-#define GET_SHORT_VERSION() CONCATENATE_WITH_DOT(MAJOR, MINOR)
-#define GET_FULL_VERSION() CONCATENATE_WITH_DOTS(MAJOR, MINOR, PATCH, BUILD_NUMBER)
-
 // Enum to represent OS types
 enum class OSTYPE
 {
