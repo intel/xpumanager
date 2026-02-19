b2942e90c7e1a6010be01e734dbdcbe346a76c72
Markus Lehtonen <markus.lehtonen@intel.com>
2026-02-19 08:26:48 +0200

    scripts/make-dockerized: build an image to run
    Makes subsequent runs faster as we cache the image. Also, allows running
    the image with the user's uid/gid.

--- scripts/make-dockerized.sh
+++ scripts/make-dockerized.sh
@@ -3,12 +3,27 @@
 LEVEL_ZERO_VERSION=${LEVEL_ZERO_VERSION:-"1.26.2"}
 L0_BASE_URL="https://github.com/oneapi-src/level-zero/releases/download"
 
-docker run --rm -v "$(pwd):/app" -w /app \
-    golang:1.25 \
-    /bin/sh -c \
-    "apt-get update && apt-get install -y --no-install-recommends unzip &&\
-     git config --global --add safe.directory /app && \
-     curl -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb \
-          -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero-devel_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb && \
-     apt-get install -y ./*.deb && \
-     make $*"
+IMAGE_TAG="xpumd-builder:latest"
+
+docker build -t "${IMAGE_TAG}" - <<EOF
+FROM golang:1.25
+RUN apt-get update && apt-get install -y --no-install-recommends unzip && \
+    curl -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb \
+         -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero-devel_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb && \
+    dpkg -i ./*.deb && \
+    rm -f ./*.deb && rm -rf /var/lib/apt/lists/*
+EOF
+
+if [ "$(ps -p 1 -o comm=)" = "systemd" ]; then
+    USER_FLAG="--user $(id -u):$(id -g)"
+else
+    echo "Probably running inside a container, running container as root"
+fi
+
+docker run --rm \
+    ${USER_FLAG} \
+    -e HOME=/go \
+    -v "$(pwd):/go/src" \
+    -w /go/src \
+    "${IMAGE_TAG}" \
+    /bin/sh -c "make $*"
