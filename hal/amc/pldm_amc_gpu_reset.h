aef447b6d60fcde395de17dece816a2335794c78
Vishnu Khanth <vishnu.khanth.b@intel.com>
2026-02-20 09:25:34 +0000

    amc: fix pldm command for gpu reset
        * Uses the correct PLDM command and updates the request payload
          structure and payload data.
        * Increases wait time before reading response from AMC.
    
    Related-To: XPUM-1091
    
    Signed-off-by: Vishnu Khanth <vishnu.khanth.b@intel.com>

--- hal/amc/pldm_amc_gpu_reset.h
+++ hal/amc/pldm_amc_gpu_reset.h
@@ -13,32 +13,36 @@
 #include "pldm_platform.h"
 
 // PLDM Platform Monitoring and Control Command Code for AMC GPU Reset (DSP0248)
-#define PLDM_SET_STATE_EFFECTER_ENABLE 0x38
-
-// Event Message Enable
-enum eventMsgEnable
-{
-	EVENT_MSG_ENABLE_EVENTS = 0x00,
-	EVENT_MSG_DISABLE_EVENTS = 0x01,
-	EVENT_MSG_NO_CHANGE = 0xFF
-};
+#define PLDM_SET_STATE_EFFECTER_STATES 0x39
 
 // AMC-specific Effecter ID for GPU reset
 #define PLDM_STATE_EFFECTER_AIC_RESET 402
 
-#pragma pack(push, 1)
-struct stateEffecterOpField
+enum setRequest
 {
-	uint8_t effecter_operational_state;
-	uint8_t event_message_enable;
+	SET_REQUEST_NO_CHANGE = 0x00,
+	SET_REQUEST_SET = 0x01
 };
 
-// SetStateEffecterEnable request payload fields as per spec (DSP0248)
-struct setStateEffecterEnableReq
+enum effecterState
+{
+	EFFECTER_STATE_REQUEST_NORMAL = 0x01,
+	EFFECTER_STATE_REQUEST_RESTART = 0x02
+};
+
+#pragma pack(push, 1)
+struct stateEffecterStateField
+{
+	uint8_t set_request;
+	uint8_t effecter_state;
+};
+
+// SetStateEffecterStates request payload fields as per spec (DSP0248)
+struct setStateEffecterStatesReq
 {
 	uint16_t effecter_id;
 	uint8_t composite_effecter_count;
-	stateEffecterOpField state_field;
+	stateEffecterStateField state_field;
 };
 #pragma pack(pop)
 
