d29b3a10d5330de92819f55c7a79caf23fe82e7e
Markus Lehtonen <markus.lehtonen@intel.com>
2026-03-06 17:10:04 +0200

    ci: fix xpumd trivy license scan
    We need to warm up the go mod cache so that trivy license scan is able
    to extract the licenses. Use a kludge'ish script for doing that as make
    (for make build) is not available in the current runner we use.
    
    Also, set options to accept (ignore) MPL-2.0 license and skip
    xpumd/tools from the scan.
    
    TODO: remove the xpumd/scripts/trivy-warmup.sh when the xpum runner has
    buildx installed and can be used in vars.RUNS_ON (for all xpumd jobs).
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>

---
+++ xpumd/scripts/trivy-warmup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+#
+# Copyright (C) 2026 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+set -o pipefail
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+OTEL_VERSION=0.146.0
+
+cd "$SCRIPT_DIR/.."
+sed "s/@OTEL_VERSION@/${OTEL_VERSION}/g" builder-config.yaml.in > builder-config.yaml
+go tool -modfile tools/go.mod builder --config=builder-config.yaml --skip-compilation
+
+for m in $(git ls-files '*/go.mod' | xargs dirname); do
+    go -C "$m" mod download
+done
