64e14ae5c197c2b580a924deb50cf78453ec01e6
Markus Lehtonen <markus.lehtonen@intel.com>
2026-04-01 08:44:13 +0300

    xpumd: bump opentelemetry collector to v0.149.0
    Dev-SHA: ed37234a6280805472663d09596aa717a8d6d763

--- xpumd/scripts/trivy-warmup.sh
+++ xpumd/scripts/trivy-warmup.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-OTEL_VERSION=0.148.0
+OTEL_VERSION=0.149.0
 
 cd "$SCRIPT_DIR/.."
 sed "s/@OTEL_VERSION@/${OTEL_VERSION}/g" builder-config.yaml.in > builder-config.yaml
