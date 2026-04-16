52694a7a9610a94e22d28c0b857cffe179ec3df1
Markus Lehtonen <markus.lehtonen@intel.com>
2026-04-16 11:03:26 +0300

    xpumd/scripts: add libyaml-dev to builder docker image
    Dev-SHA: 3b600ee7c015410d2444f5602d85c83b4fa46b63

--- xpumd/scripts/make-dockerized.sh
+++ xpumd/scripts/make-dockerized.sh
@@ -20,7 +20,7 @@ RUN mkdir -p /go/pkg && chmod 777 /go/pkg
 
 # Unzip needed for installing protoc, doxygen for generating Go bindings
 RUN apt-get update && apt-get install -y --no-install-recommends \
-        unzip doxygen libcyaml-dev && \
+        unzip doxygen libyaml-dev libcyaml-dev && \
     curl -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb \
          -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/level-zero-devel_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb && \
     dpkg -i ./*.deb && \
