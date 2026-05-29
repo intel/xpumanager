5d530dd0a827887a40ef97b8cc6e051b70788ae8
Eero Tamminen <eero.t.tamminen@intel.com>
2026-05-29 12:47:12 +0300

    xpumd/build: verify checksums for the directly downloaded DEB packages
    Dev-SHA: 88f1bdfbdbd0fbec0b504cf004f6e171f5efbbba

--- xpumd/scripts/make-dockerized.sh
+++ xpumd/scripts/make-dockerized.sh
@@ -10,23 +10,36 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
+# checksum file for L0 loader DEB packages
+CHECKSUMS=checksums-loader.txt
+
 LEVEL_ZERO_VERSION_DEFAULT=$(sed -n 's/^ARG LEVEL_ZERO_VERSION=//p' "${SCRIPT_DIR}/../Dockerfile")
 LEVEL_ZERO_VERSION=${LEVEL_ZERO_VERSION:-"${LEVEL_ZERO_VERSION_DEFAULT}"}
 L0_BASE_URL="https://github.com/oneapi-src/level-zero/releases/download"
 
 IMAGE_TAG="xpumd-builder:latest"
 
-docker build -t "${IMAGE_TAG}" - <<EOF
+docker build -t "${IMAGE_TAG}" -f - "${SCRIPT_DIR}/../level-zero-go/level-zero" <<EOF
 FROM golang:1.26
 # Pre-create /go/pkg with wide permissions to allow "docker run" below (with non-root user)
 # to write to it (the mount (-v) in docker run would otherwise create it with 755)
 RUN mkdir -p /go/pkg && chmod 777 /go/pkg
+
+COPY $CHECKSUMS .
 
 # Unzip needed for installing protoc, doxygen for generating Go bindings
 RUN apt-get update && apt-get install -y --no-install-recommends \
         unzip doxygen libyaml-dev libcyaml-dev jq clang-format && \
     curl -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/libze1_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb \
          -LO ${L0_BASE_URL}/v${LEVEL_ZERO_VERSION}/libze-dev_${LEVEL_ZERO_VERSION}+u24.04_amd64.deb && \
+    if ! sha256sum --check $CHECKSUMS ; then \
+	echo "--- Expected ($CHECKSUMS): ---"; \
+	cat "$CHECKSUMS"; \
+	echo "--- Got: ---"; \
+	sha256sum *.deb; \
+	echo "=> ERROR"; \
+	exit 1; \
+    fi; \
     dpkg -i ./*.deb && \
     rm -f ./*.deb && rm -rf /var/lib/apt/lists/*
 EOF
