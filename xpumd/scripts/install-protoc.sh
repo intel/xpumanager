45997cfee4844d8be74476dc9792dc9969b85073
Eero Tamminen <eero.t.tamminen@intel.com>
2026-05-29 14:56:06 +0300

    xpumd/build: verify checksum for the downloaded protoc ZIP file
    Dev-SHA: ceb8d63fc3ad7deb6da9e1c6ab288821405954d4

--- xpumd/scripts/install-protoc.sh
+++ xpumd/scripts/install-protoc.sh
@@ -7,11 +7,15 @@
 
 set -o pipefail
 
-PB_VERSION=${PB_VERSION-33.4}
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 
+PB_VERSION="${PB_VERSION-33.4}"
+ZIP_FILE="protoc-$PB_VERSION-linux-x86_64.zip"
+CHECKSUM="${CHECKSUM-c0040ea9aef08fdeb2c74ca609b18d5fdbfc44ea0042fcfbfb38860d35f7dd66}"
+
 # We operate under the tools go module
-cd tools
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd "$SCRIPT_DIR/../tools"
 BIN_DIR=$(realpath bin)
 
 if [ -x "$BIN_DIR/protoc" ]; then
@@ -21,11 +25,17 @@ fi
 if [ "$INSTALLED_PROTOC_VERSION" == "$PB_VERSION" ]; then
     echo "found protoc version $INSTALLED_PROTOC_VERSION installed in $BIN_DIR"
 else
-    curl -LO "$PB_REL/download/v$PB_VERSION/protoc-$PB_VERSION-linux-x86_64.zip"
+    curl -LO "$PB_REL/download/v$PB_VERSION/$ZIP_FILE"
+    if ! echo "$CHECKSUM $ZIP_FILE" | sha256sum --check -; then
+	echo "--- Checksum ERROR, expected: ---"
+	echo "$CHECKSUM  $ZIP_FILE"
+	echo "--- Got: ---"
+	sha256sum "$ZIP_FILE"
+	exit 1
+    fi
 
-    unzip -o "protoc-$PB_VERSION-linux-x86_64.zip"
-
-    rm -f "protoc-$PB_VERSION-linux-x86_64.zip"
+    unzip -o "$ZIP_FILE"
+    rm -f "$ZIP_FILE"
     rm -f readme.txt
 fi
 
