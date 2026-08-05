56905b9db005c9089c8704287bb6b1a30c6485b6
Markus Lehtonen <markus.lehtonen@intel.com>
2026-08-05 11:10:01 +0300

    xpumd/level-zero-go: add tooling to generate hack/tools.go
    Copy what was done for the tools module in the XPUM Daemon: add
    infrastructure to automatically generate tools.go (now in the hack/
    module). The purpose of the to-be-auto-generated file is to make all go
    tools (listed as tools() in the go.mod) direct dependencies and thus
    updateable by dependabot. With auto-generation the go.mod is the single
    source of truth.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>
    
    Dev-SHA: db95d6a7e5bb4c996c7a16bb4f063ef260ccc5de

---
+++ xpumd/level-zero-go/hack/doc.go
@@ -0,0 +1,9 @@
+//
+// Copyright (C) 2026 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+// Package hack contains scripts/tools for building the project.
+//
+//go:generate ./generate-tools.sh
+package hack
