d1db364c8855309178004b7daab90715ff6f0356
Markus Lehtonen <markus.lehtonen@intel.com>
2026-08-13 14:43:04 +0300

    xpumd: allow running a subset of tests
    Add WHAT variable to the Makefile to enable (easy) selection of which
    tests to run. Applies both to unit tests and the integration tests.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>
    
    Dev-SHA: 780808d1268dedf47f0bc28894a20d2326a8a761

--- xpumd/scripts/run-integration-tests.sh
+++ xpumd/scripts/run-integration-tests.sh
@@ -8,5 +8,8 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 TIMEOUT=${TIMEOUT:-10m}
 
+# RUN_REGEX can be used to select which tests to run, e.g. RUN_REGEX=TestMetrics
+RUN_REGEX=${RUN_REGEX:-.}
+
 exec go -C "${SCRIPT_DIR}/../test/integration" test -count=1 -v \
-    -timeout "${TIMEOUT}" ./k8s -args "$@"
+    -timeout "${TIMEOUT}" -run "${RUN_REGEX}" ./k8s -args "$@"
