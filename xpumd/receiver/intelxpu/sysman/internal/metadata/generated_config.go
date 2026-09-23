c658df6fc9f45d04ea0ba49adf570fffa0051e7c
Markus Lehtonen <markus.lehtonen@intel.com>
2026-09-23 15:45:02 +0300

    xpumd: update auto-generated code after otelcol version bump
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>
    
    Dev-SHA: 650123ef1c2246d5d7ccec5d2244e802c5edbcd0

--- xpumd/receiver/intelxpu/sysman/internal/metadata/generated_config.go
+++ xpumd/receiver/intelxpu/sysman/internal/metadata/generated_config.go
@@ -1557,8 +1557,3 @@ func NewDefaultMetricsBuilderConfig() MetricsBuilderConfig {
 		Metrics: DefaultMetricsConfig(),
 	}
 }
-
-// Deprecated: Use NewDefaultMetricsBuilderConfig.
-func DefaultMetricsBuilderConfig() MetricsBuilderConfig {
-	return NewDefaultMetricsBuilderConfig()
-}
