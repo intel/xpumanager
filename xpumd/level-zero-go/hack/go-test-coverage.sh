a4320b84938dd0ee7810777b259e77503ea56c33
Markus Lehtonen <markus.lehtonen@intel.com>
2026-09-25 11:29:07 +0300

    xpumd/level-zero-go/hack/go-test-coverage: fix for nested modules
    Instrument only the packages of the module itself. Previously we used
    "-coverpkg=./..." which included also nested submodules. We test/report
    every module individually so it's cleaner to keep the coverage data of nested
    submodules out from the parent module report (this is how it would work
    by default without the "-coverpkg", which we add to also report stuff
    that don't have any tests).
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>
    
    Dev-SHA: 454ba1e49db59c098780347888ad9cdf60bab9e2

--- xpumd/level-zero-go/hack/go-test-coverage.sh
+++ xpumd/level-zero-go/hack/go-test-coverage.sh
@@ -32,14 +32,19 @@ for module in "$@"; do
     profile="${COVERAGE_DIR}/${module}/coverage.out"
     mkdir -p "$(dirname "${profile}")"
 
-    # -coverpkg=./... to cover  all packages, also those without tests.
+    # comma-separated list of packages given to -coverpkg, to cover also module dependencies without tests.
+    if ! packages=$(go -C "${module}" list ./... | paste -sd,); then
+        error=1
+        continue
+    fi
+
     # -count=1 to prevent caching of test results, needed because of the
     # stub driver, as the Go test cache misses (at least) the following things:
     # 1. PKG_CONFIG_PATH trick (a change in the stub driver is not tracked)
     # 2. the testdata/ files are read by the stub driver (not the Go code)
     # shellcheck disable=SC2086  # GO_TEST_FLAGS is a list of flags
     go -C "${module}" test ${GO_TEST_FLAGS} -count=1 \
-       -coverpkg=./... -coverprofile="${profile}" ./... || error=1
+       -coverpkg="${packages}" -coverprofile="${profile}" ./... || error=1
 
     # The last line of the report is the total coverage of the module. Capture
     # the report so that a failure of "go tool cover" is not masked by "tail".
